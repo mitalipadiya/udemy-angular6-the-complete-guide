2367dd6a7697c93c61b6b038ef02fb01070d9c69
mitalipadiya <mdpadiya10@gmail.com>
2019-03-10 16:39:07 +0530

    10-07 adding ingredients to recipes

--- shopping-app/src/app/recipes/recipe.service.ts
+++ shopping-app/src/app/recipes/recipe.service.ts
@@ -1,11 +1,20 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes:Recipe[] = [
-    new Recipe('A test recipe', 'test', 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fnatashaskitchen.com%2Fwp-content%2Fuploads%2F2017%2F08%2FAvocado-Shrimp-Salsa-Recipe-4.jpg&f=1'),
-    new Recipe('A test recipe2', 'test', 'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fnatashaskitchen.com%2Fwp-content%2Fuploads%2F2017%2F08%2FAvocado-Shrimp-Salsa-Recipe-4.jpg&f=1')
+    new Recipe('Sandwitch',
+    'Enjoy the stuff between two breads',
+    'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fnatashaskitchen.com%2Fwp-content%2Fuploads%2F2017%2F08%2FAvocado-Shrimp-Salsa-Recipe-4.jpg&f=1',
+    [new Ingredient("Bread",2),
+  new Ingredient("salad",4)]),
+    new Recipe('Burger',
+    "Don't forget to try it",
+    'https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fnatashaskitchen.com%2Fwp-content%2Fuploads%2F2017%2F08%2FAvocado-Shrimp-Salsa-Recipe-4.jpg&f=1',
+  [new Ingredient("Bun", 2),
+new Ingredient("Vegetables",5)])
   ];
 
   getRecipes(){
